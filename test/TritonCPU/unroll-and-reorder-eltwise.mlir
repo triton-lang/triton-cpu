8aa398cb7584078a5bbca40c2539274d337ba859
Julian Oppermann <julian.oppermann@intel.com>
2026-07-18 08:48:23 -0700

    [TTTCIR] Preserve original order when unrolling elementwise ops

--- test/TritonCPU/unroll-and-reorder-eltwise.mlir
+++ test/TritonCPU/unroll-and-reorder-eltwise.mlir
@@ -164,3 +164,69 @@ tt.func public @negative_eltwise_kernel4(%arg0: !tt.ptr<bf16>, %arg2: !tt.ptr<bf
   vector.transfer_write %16, %21[%9, %10] {in_bounds = [true, true]} : vector<32x32xbf16>, memref<?x?xbf16, strided<[?, 1]>>
   tt.return
 }
+
+// -----
+
+// Check reordering in the presence of multiple vector.transfer_read ops.
+
+// CHECK-LABEL: eltwise_kernel5
+// CHECK:         vector.transfer_read
+// AVX512-SAME:   vector<8x32xbf16>
+// AVX2-SAME:     vector<2x32xbf16>
+// CHECK:         arith.extf
+// AVX512-SAME:   vector<8x32xf32>
+// AVX2-SAME:     vector<2x32xf32>
+// CHECK:         vector.transfer_read
+// AVX512-SAME:   vector<8x32xbf16>
+// AVX2-SAME:     vector<2x32xbf16>
+// CHECK:         arith.extf
+// AVX512-SAME:   vector<8x32xf32>
+// AVX2-SAME:     vector<2x32xf32>
+// CHECK:         arith.addf
+// AVX512-SAME:   vector<8x32xf32>
+// AVX2-SAME:     vector<2x32xf32>
+// CHECK:         arith.truncf
+// AVX512-SAME:   vector<8x32xf32> to vector<8x32xbf16>
+// AVX2-SAME:     vector<2x32xf32> to vector<2x32xbf16>
+// CHECK:         vector.transfer_write
+// AVX512-SAME:   vector<8x32xbf16>
+// AVX2-SAME:     vector<2x32xbf16>
+
+// Pattern shall repeat.
+
+// CHECK:         vector.transfer_read
+// CHECK:         arith.extf
+// CHECK:         vector.transfer_read
+// CHECK:         arith.extf
+// CHECK:         arith.addf
+// CHECK:         arith.truncf
+// CHECK:         vector.transfer_write
+
+tt.func public @eltwise_kernel5(%arg0: !tt.ptr<bf16>, %arg1: !tt.ptr<bf16>, %arg2: !tt.ptr<bf16>, %arg3: i32, %arg4: i32) {
+  %cst = arith.constant 0.000000e+00 : bf16
+  %cst_0 = arith.constant dense<2.000000e+00> : vector<32x32xf32>
+  %cst_1 = arith.constant dense<1.500000e+00> : vector<32x32xf32>
+  %c1_i64 = arith.constant 1 : i64
+  %c32_i32 = arith.constant 32 : i32
+  %0 = tt.get_program_id x : i32
+  %1 = arith.muli %0, %c32_i32 : i32
+  %2 = tt.get_program_id y : i32
+  %3 = arith.muli %2, %c32_i32 : i32
+  %4 = arith.extsi %arg4 : i32 to i64
+  %5 = tt.make_tensor_descriptor %arg0, [%arg3, %arg4], [%4, %c1_i64] : <bf16>, <32x32xbf16>
+  %6 = tt.make_tensor_descriptor %arg1, [%arg3, %arg4], [%4, %c1_i64] : <bf16>, <32x32xbf16>
+  %7 = tt.make_tensor_descriptor %arg2, [%arg3, %arg4], [%4, %c1_i64] : <bf16>, <32x32xbf16>
+  %8 = triton_cpu.extract_memref %5 : <32x32xbf16> -> memref<?x?xbf16, strided<[?, 1]>>
+  %9 = arith.index_cast %1 : i32 to index
+  %10 = arith.index_cast %3 : i32 to index
+  %11 = vector.transfer_read %8[%9, %10], %cst {in_bounds = [true, true]} : memref<?x?xbf16, strided<[?, 1]>>, vector<32x32xbf16>
+  %12 = arith.extf %11 : vector<32x32xbf16> to vector<32x32xf32>
+  %13 = triton_cpu.extract_memref %6 : <32x32xbf16> -> memref<?x?xbf16, strided<[?, 1]>>
+  %14 = vector.transfer_read %13[%9, %10], %cst {in_bounds = [true, true]} : memref<?x?xbf16, strided<[?, 1]>>, vector<32x32xbf16>
+  %15 = arith.extf %14 : vector<32x32xbf16> to vector<32x32xf32>
+  %17 = arith.addf %12, %15 : vector<32x32xf32>
+  %20 = arith.truncf %17 : vector<32x32xf32> to vector<32x32xbf16>
+  %21 = triton_cpu.extract_memref %7 : <32x32xbf16> -> memref<?x?xbf16, strided<[?, 1]>>
+  vector.transfer_write %20, %21[%9, %10] {in_bounds = [true, true]} : vector<32x32xbf16>, memref<?x?xbf16, strided<[?, 1]>>
+  tt.return
+}
--- third_party/cpu/lib/TritonCPUTransforms/UnrollAndReorderElementwiseOps.cpp
+++ third_party/cpu/lib/TritonCPUTransforms/UnrollAndReorderElementwiseOps.cpp
@@ -173,6 +173,41 @@ static void buildElementwiseDAG(Operation *op, VRFInfo &vrfInfo,
 }
 
 static constexpr auto *unrollShapeAttrName = "unroll_shape";
+static constexpr auto *unrollOrderAttrName = "unroll_order";
+
+namespace {
+// The upstream vector unroll patterns don't always propagate discardable
+// attributes to the unrolled ops. This should be fixed upstream, but until
+// then, this listener looks for the canonical chain of insert_strided_slice ops
+// and propagates the unroll_order attribute to the clones.
+struct PropagateOrderAttrListener : public virtual RewriterBase::Listener {
+  template <typename OpTy>
+  void propagateAttrToUnrolledOps(Operation *op, IntegerAttr attr,
+                                  Value newValue) {
+    if (auto oldOp = dyn_cast<OpTy>(op)) {
+      Value it = newValue;
+      vector::InsertStridedSliceOp insertOp;
+      OpTy newOp;
+      while ((insertOp = it.getDefiningOp<vector::InsertStridedSliceOp>()) &&
+             (newOp = insertOp.getValueToStore().getDefiningOp<OpTy>())) {
+        newOp->setAttr(unrollOrderAttrName, attr);
+        it = insertOp.getDest();
+      }
+    }
+  }
+
+  void notifyOperationReplaced(Operation *op, ValueRange newValues) override {
+    auto unrollOrderAttr = op->getAttrOfType<IntegerAttr>(unrollOrderAttrName);
+    if (!unrollOrderAttr || newValues.size() != 1)
+      return;
+    Value newValue = newValues.front();
+    propagateAttrToUnrolledOps<vector::TransferReadOp>(op, unrollOrderAttr,
+                                                       newValue);
+    propagateAttrToUnrolledOps<vector::ShapeCastOp>(op, unrollOrderAttr,
+                                                    newValue);
+  }
+};
+} // namespace
 
 static LogicalResult unrollOpsIn(scf::ExecuteRegionOp exec) {
   vector::UnrollVectorOptions unrollOptions;
@@ -194,8 +229,11 @@ static LogicalResult unrollOpsIn(scf::ExecuteRegionOp exec) {
 
   RewritePatternSet patterns(exec.getContext());
   vector::populateVectorUnrollPatterns(patterns, unrollOptions);
+  GreedyRewriteConfig config;
+  PropagateOrderAttrListener listener;
+  config.setListener(&listener);
   return applyPatternsGreedily(exec->getParentOfType<triton::FuncOp>(),
-                               std::move(patterns));
+                               std::move(patterns), config);
 }
 
 static void rewriteElementwiseDAG(vector::TransferWriteOp writeOp,
@@ -233,13 +271,15 @@ static void rewriteElementwiseDAG(vector::TransferWriteOp writeOp,
   Block *execBlock = rewriter.createBlock(&exec.getRegion());
   rewriter.setInsertionPointToStart(execBlock);
 
-  // Clone ops into the execute_region and determine unrolling shape.
+  // Clone ops into the execute_region.
   IRMapping mapping;
+  unsigned order = 0;
   for (auto *op : ops) {
     Operation *cloned = rewriter.clone(*op, mapping);
     auto rank = getVectorType(cloned).getRank();
     cloned->setAttr(unrollShapeAttrName,
                     rewriter.getI64ArrayAttr(unrollShape.take_back(rank)));
+    cloned->setAttr(unrollOrderAttrName, rewriter.getI64IntegerAttr(order++));
   }
 
   // Insert terminator and replace the original write op with the
@@ -251,39 +291,51 @@ static void rewriteElementwiseDAG(vector::TransferWriteOp writeOp,
   [[maybe_unused]] auto res = unrollOpsIn(exec);
   assert(succeeded(res));
 
-  // Remove the unroll_shape attribute and reorder ops in the execute_region.
-  // The idea is to produce chains from sources to the sinks (= unrolled
-  // transfer_write ops) that do not exceed the VRF's capacity.
-  SmallVector<Operation *> unrolledOps;
-  llvm::transform(execBlock->getOperations(), std::back_inserter(unrolledOps),
-                  [](Operation &op) { return &op; });
-  Operation *lastInvOp = nullptr;
-  for (auto *unrOp : unrolledOps) {
-    if (unrOp == execBlock->getTerminator())
+  // Sort the ops into buckets, according to their unroll_order attribute:
+  // 0:    Ops that don't have an order attribute and are not
+  //       vector.transfer.write ops.
+  // 1..N: Ops that have an order attribute.
+  // N+1:  vector.transfer.write ops.
+  SmallVector<SmallVector<Operation *>> buckets(order + 2);
+  Operation *terminator = execBlock->getTerminator();
+  for (Operation &op : execBlock->getOperations()) {
+    if (&op == terminator)
       continue;
-
-    unrOp->removeAttr(unrollShapeAttrName);
-
-    Operation *lastOperandOp = nullptr;
-    for (Value operand : unrOp->getOperands()) {
-      Operation *operandOp = operand.getDefiningOp();
-      if (!operandOp || operandOp->getBlock() != execBlock)
-        continue;
-      if (!lastOperandOp || lastOperandOp->isBeforeInBlock(operandOp))
-        lastOperandOp = operandOp;
+    if (isa<vector::TransferWriteOp>(op)) {
+      buckets.back().push_back(&op);
+      continue;
     }
-    if (lastOperandOp)
-      unrOp->moveAfter(lastOperandOp);
-    else {
-      // Make sure invariant ops moved to the top remain in their original
-      // order.
-      if (lastInvOp)
-        unrOp->moveAfter(lastInvOp);
-      else
-        unrOp->moveBefore(&execBlock->front());
-      lastInvOp = unrOp;
-    }
+
+    auto orderAttr = op.getAttrOfType<IntegerAttr>(unrollOrderAttrName);
+    unsigned order = orderAttr ? orderAttr.getInt() + 1 : 0;
+    buckets[order].push_back(&op);
   }
+
+  // We'll bring ops in the desired order by subsequently moving them before the
+  // terminator.
+  for (auto *op : buckets.front())
+    op->moveBefore(terminator);
+
+  // Remove special buckets: The first one we just handled, and any empty
+  // buckets (that corresponded to constants or folded operations).
+  buckets.erase(buckets.begin());
+  buckets.erase(std::remove_if(buckets.begin(), buckets.end(),
+                               [](auto &bucket) { return bucket.empty(); }),
+                buckets.end());
+  
+  // All remaining buckets should contain the same number of ops, equivalent to the unroll factor.
+  unsigned unrollFactor = buckets.back().size();
+  assert(llvm::all_of(
+      buckets, [&](auto &bucket) { return bucket.size() == unrollFactor; }));
+
+  for (unsigned u = 0; u < unrollFactor; ++u)
+    for (unsigned b = 0; b < buckets.size(); ++b) {
+      Operation *op = buckets[b][u];
+      op->moveBefore(terminator);
+      op->removeAttr(unrollShapeAttrName);
+      op->removeAttr(unrollOrderAttrName);
+    }
+
   LDBG("  Success.");
 }
 
