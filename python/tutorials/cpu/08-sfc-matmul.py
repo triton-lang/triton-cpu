dcec003239630e26d2a89c114128e6de6dcf53f3
Julian Oppermann <julian.oppermann@intel.com>
2026-09-07 01:47:48 -0700

    Improve comment

--- python/tutorials/cpu/08-sfc-matmul.py
+++ python/tutorials/cpu/08-sfc-matmul.py
@@ -84,8 +84,9 @@ def block_transpose_pack_kernel(a_in_ptr, a_out_ptr, a_sfc_map_ptr, b_in_ptr, b_
 #
 # Each program computes a single output tile with the 2D coordinates derived from the precomputed SFC mapping.
 # If `BLOCKING_FACTOR_K == 1`, then program handles all `BLOCKS_K = K // BLOCK_SIZE_K` blocks along the common dimension,
-# otherwise the program performs a partial accumulation of the K blocks in the half-open interval:
-#    [ ik * (BLOCKS_K // BLOCKING_FACTOR_K), (ik + 1) * (BLOCKS_K // BLOCKING_FACTOR_K) )
+# otherwise the program performs a partial accumulation of `BLOCKS_K_PER_PROG = ⌈BLOCKS_K / BLOCKING_FACTOR_K⌉` blocks,
+# starting at `ik * BLOCKS_K_PER_PROG` and ending at `min((ik + 1) * BLOCKS_K_PER_PROG, BLOCKS_K)`. The partial
+# accumulation is stored in `c_tmp_ptr` and will be loaded and accumulated in the next iteration of the outer loop.
 #
 @triton.jit
 def sfc_kernel(
